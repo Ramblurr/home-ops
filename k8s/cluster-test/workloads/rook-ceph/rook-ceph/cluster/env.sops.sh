4620bbca33cc249ed2972dbc0f7f0f1b299be773
Casey <unnamedrambler@gmail.com>
2023-03-15 11:42:49 +0100

    cluster-test

---
+++ k8s/cluster-test/workloads/rook-ceph/rook-ceph/cluster/env.sops.sh
@@ -0,0 +1,26 @@
+{
+    "data": "ENC[AES256_GCM,data:AlvzEveN2dIgUVonFvtJ54tYKc3eJssR6EUeKwEt3gyrIfWaTX18MYnuLnhFET3szB9mkqJH0eHU3BcWzpZi5rTIORQskC3necl20Qx2YfBVBXMmvZ8dThtWBuJnUOlFpOXtet+8R8j/EYs5pc6RH4asdFFakTVySx8MM7w0hQwdiWZQfhU+dae4erHYpub3EPFD/Fboi1lKCOJxAsaI3KL8RtkBahQlvj5+vgugo0dgkDyQnlIgha3l4dgM7ViSV7WkWm+NY+P+s4/cYfBaK6awoiQaq41F6iuTpKKmG2WawDmxjG923scI3Du6Z+3TqN8nDkTu/B/VBs2SEXZGEZoiuN+hfHcfrRPQ1hgFXMMkcn5ylKRfXLVyGkCCMCKLtilrfxD2BRiiWBbQMw8f/lBH3UUV3KPTRV3ZBIeodBqSsB0AQlRYZope5xt+1Ev0W8El7za9qHYmmt3114Bd88g8McpDL4AkoObe1H6/JqwI0hLqrI0aZaa6wbeBWNkMao/bGPzf7lbMVMgApf4UcI6nMHVMc1/M96vUBJvr0mbdGnsCOttVOrd0JC8FhjNhBNrjzPRgXVnfm0s28/Ix/EFeFyCvOrI7BKBsMWGsMKPtQfAwN9UYiC03C6CKLB1C45LWIBsUTTpViK/tMUkoTLb/NIUxI56Hrqxsaapay4Zf3ZVybbzc+Z/BXMqu59peNkO8pTRjHMkjiL8eTlnxAStRn7pl6KRkv1jTrAEH1XJRLEcvgV7PcYEKcHJBDeUSrlw+N8rTAth7bc+X/nGpO2h2bS8etJ736Em9oTIGLiSRDnw8T8FnsbJlZ9tdUsWc6z4ZiQbQyoHL173oDIBTz79BQrboQG4VugvUbr7VQkreFeD5aPRRbTNposkBo+JwjQkbPiWtTyczCXXslPqahTMryije9eYpX5M1b+kPgclzuy6uWy519FL40lV0OeX0IZqXiTTLZlra4ixyrZHHZYP6Rxsy4nJ/80RAE23dv97uIrpZ+lAoaI0kwX3NY6jm/UARoffU6FM3rlpG0JwW3joMVVX8i/2d5WHHFlWbLu1bUfuFo7VMg35u4EIBsC4vzMHuoo0mzQ5LR95wpxrMK95qQfThFIGcB0AoGWT68tKLQNk7IuBkW85jxiTYUgL9qq/cguvEn8Zk0BSuujjyWGEYo8QCH/BnKPkRnGF5gT7eA/T+OIhY6wRfYmwNnvLsnMUvczK6QTVjlO5blhXK6VIOsIjrubABM2SCAuePZSN94pnrT/P3iVuWXA8KKt1ge62Bb0UJg2s/fTIjcEt+oRzsyU/0kMke9g/UoecgnTl4Sdtk/mO1s6DvpTorzYm3njGXffkyBWsVv0gHH4808iyluOUnulaRCv0Qc3zogae5ugaHS+Im0A==,iv:/dNn1NKpkPxcT3OxhYHz2uRnKdUMdzHO/Iu/FuGGMvQ=,tag:rlLakoQx3Ml8vPnjUT5/RA==,type:str]",
+    "sops": {
+        "kms": null,
+        "gcp_kms": null,
+        "azure_kv": null,
+        "hc_vault": null,
+        "age": [
+            {
+                "recipient": "age15j42dspmmwprjau6l48xp05d97s8ml5s3tjxrfwvm37tvuynssuqtsevkj",
+                "enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBBL1pRY2ZZd0NPN2ltUFVS\neHl2b2I1NlF4aC9nYXQva2pjWkQ1VGNlOGx3CnI1Qit0VGl4WitNdmMvaGRpUk90\nRTkyQm5tR2dyOXdkY0JXd1pBNUVKUncKLS0tIEpPcDdmbWRDdTVVUDRPTnF4Znhz\nbUphNGl4QXVKU0o2NE5Yand5K21jT1kKwKV5IpOvpbqnUk/Hpa8TRoy5Zmebvz0l\njnOVHncOFemoXTm4Uu0v5+yMdTOOC255eclGcgKJKadCpNggc2AcvQ==\n-----END AGE ENCRYPTED FILE-----\n"
+            }
+        ],
+        "lastmodified": "2023-02-24T16:25:46Z",
+        "mac": "ENC[AES256_GCM,data:/o1MsITGzf8pzniKm61hLYVru0QUqvYQ/SsysTMbEfC4HEv8uVaW9luGZMf+GscVFSi6cYvLOoR5+IW2PRHqvE5x1zr72VBUCHxhHYgltd/tp1TpobkAYkIEXWVKq632hqwjNpjwCuCcDzFiS6wOKnJX7RGmgGFlr41lP3ALFjc=,iv:u2FerqsbTEpOcUb0vrh7bd2+Jf/7uf+NCHGJ1+if1cE=,tag:qxJom3zLlpCTkoHIeGXqtQ==,type:str]",
+        "pgp": [
+            {
+                "created_at": "2023-02-24T15:48:07Z",
+                "enc": "-----BEGIN PGP MESSAGE-----\n\nwcFMAz/c2vn977oWARAAywms0gqrd13Gaqn7N8qEbKmBJpgwu2rgY8xObhTHqwFQ\nBc6afIjhJt+Uaxc/QJCGl5CtBrOJt5TL0ihLCZ+cB3g/noCNVkR0YmfM67d7dMWd\n1lr9nwwu0iPtHhaS60f2Xk7TNROxhz62D4TRu2fdi06eW8+VsHgrVDud4gGV3ioF\n99928dq6EL5EGdG3n81hVHmHoCMhgXt5Y9+V2OKGDdtFhApce3tb1DkFwRxZiwsC\nwlCFngB2w+ukaw81b0oIOvvd1Dsg8XtLlFyHO4AzcTDFJkAO3+AnzwZwuwD0AwEv\nSDDTH4xmejverz2Uz27DjOpoXauNuNHT2wbcPvChjnrih4gxOIfN+PQbFBaiC5pG\n/5odKUUDNdqebvI2uslXhYpWpNbYDz7HD0LtN0+qFm92nJtNqCwzZ3gixvLvlQT0\nm+mK2QeTprwYq6XK7N94Kw4jibhaqFfgMRx5JS2HuxXgH1B8K8QyYglxOu/0BJi+\nHSvO20iOLjfKigkK92Ym/T+KH+JllGH1h9XL5HK80ZVRVpCDYRpaha2ishZpmp7H\nZEuwevn7twjGNr/btzdBV0g55Kd11Pt8DlNe40zy4VlE4dU8pyVcpWmj7uagc7bt\nT5eIRpw4F1z3JQKjubVd7SuOE6eZnvhhIqUalMPRUA9gb1gdx0qdo0HHaFF9TGzS\nUQF4sTZn4d+0NeBHnCAAaNLw6G5948yK2FGaHLRFYfy81d7NcDJLZyQMmFiYMF6c\nvDgkZ6PkSy/rwo061f/QhpC3PnMU3OHWgkHHzLVzxFIeKQ==\n=nVx7\n-----END PGP MESSAGE-----",
+                "fp": "978C4D08058BA26EB97CB51820782DBCACFAACDA"
+            }
+        ],
+        "unencrypted_suffix": "_unencrypted",
+        "version": "3.7.3"
+    }
+}
