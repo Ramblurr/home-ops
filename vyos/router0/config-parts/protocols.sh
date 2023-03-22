e97a2e4eff70d467c92f606fb9c4cf2a814f87a3
Casey <unnamedrambler@gmail.com>
2023-03-22 14:03:08 +0100

    vyos: add bgp peering with test cluster

--- vyos/router0/config-parts/protocols.sh
+++ vyos/router0/config-parts/protocols.sh
@@ -1,4 +1,5 @@
 #!/bin/vbash
+# shellcheck shell=bash
 
 # ISP WAN
 set protocols static route 0.0.0.0/0 next-hop "${SECRET_STATIC_WAN_GATEWAY}"
@@ -18,14 +19,14 @@ set protocols static route 192.168.8.0/22 next-hop 10.9.4.27
 set protocols static route 100.64.0.0/10 next-hop 10.9.4.27
 
 # BGP configuration
-#set protocols bgp neighbor 10.1.1.31 address-family ipv4-unicast
-#set protocols bgp neighbor 10.1.1.31 description 'delta'
-#set protocols bgp neighbor 10.1.1.31 remote-as '64512'
-#set protocols bgp neighbor 10.1.1.32 address-family ipv4-unicast
-#set protocols bgp neighbor 10.1.1.32 description 'enigma'
-#set protocols bgp neighbor 10.1.1.32 remote-as '64512'
-#set protocols bgp neighbor 10.1.1.33 address-family ipv4-unicast
-#set protocols bgp neighbor 10.1.1.33 description 'felix'
-#set protocols bgp neighbor 10.1.1.33 remote-as '64512'
-#set protocols bgp parameters router-id '10.1.0.1'
-#set protocols bgp system-as '64512'
+set protocols bgp neighbor 10.9.9.101 address-family ipv4-unicast
+set protocols bgp neighbor 10.9.9.101 description 'cluster-test-cp0'
+set protocols bgp neighbor 10.9.9.101 remote-as '64512'
+set protocols bgp neighbor 10.9.9.102 address-family ipv4-unicast
+set protocols bgp neighbor 10.9.9.102 description 'cluster-test-cp1'
+set protocols bgp neighbor 10.9.9.102 remote-as '64512'
+set protocols bgp neighbor 10.9.9.103 address-family ipv4-unicast
+set protocols bgp neighbor 10.9.9.103 description 'cluster-test-cp2'
+set protocols bgp neighbor 10.9.9.103 remote-as '64512'
+set protocols bgp parameters router-id '192.168.1.1'
+set protocols bgp system-as '64512'
