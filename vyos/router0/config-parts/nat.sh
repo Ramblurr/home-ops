45ffcbb371ba6a3365f6689aeb742b0fd3d9dfe3
Casey Link <unnamedrambler@gmail.com>
2023-05-24 14:15:45 +0200

    go go cloudflared

--- vyos/router0/config-parts/nat.sh
+++ vyos/router0/config-parts/nat.sh
@@ -6,14 +6,14 @@ set nat destination rule 100 description 'HTTPS'
 set nat destination rule 100 destination port '443'
 set nat destination rule 100 inbound-interface 'eth0'
 set nat destination rule 100 protocol 'tcp'
-set nat destination rule 100 translation address "${SECRET_INGRESS_IP}"
+set nat destination rule 100 translation address "10.10.12.1"
 set nat destination rule 100 translation port '443'
 
 set nat destination rule 101 description 'HTTP'
 set nat destination rule 101 destination port '80'
 set nat destination rule 101 inbound-interface 'eth0'
 set nat destination rule 101 protocol 'tcp'
-set nat destination rule 101 translation address "${SECRET_INGRESS_IP}"
+set nat destination rule 101 translation address "10.10.12.1"
 set nat destination rule 101 translation port '80'
 
 
