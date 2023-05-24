3db368f2bbce1d9c6182a98e2dd879be59135a9c
Casey Link <unnamedrambler@gmail.com>
2023-05-24 16:42:38 +0200

    remove ingress-nginx-external

--- vyos/router0/config-parts/nat.sh
+++ vyos/router0/config-parts/nat.sh
@@ -2,19 +2,19 @@
 # shellcheck shell=bash
 
 # Forward HTTP(S) to ingress
-set nat destination rule 100 description 'HTTPS'
-set nat destination rule 100 destination port '443'
-set nat destination rule 100 inbound-interface 'eth0'
-set nat destination rule 100 protocol 'tcp'
-set nat destination rule 100 translation address "10.10.12.1"
-set nat destination rule 100 translation port '443'
-
-set nat destination rule 101 description 'HTTP'
-set nat destination rule 101 destination port '80'
-set nat destination rule 101 inbound-interface 'eth0'
-set nat destination rule 101 protocol 'tcp'
-set nat destination rule 101 translation address "10.10.12.1"
-set nat destination rule 101 translation port '80'
+# set nat destination rule 100 description 'HTTPS'
+# set nat destination rule 100 destination port '443'
+# set nat destination rule 100 inbound-interface 'eth0'
+# set nat destination rule 100 protocol 'tcp'
+# set nat destination rule 100 translation address "10.10.12.1"
+# set nat destination rule 100 translation port '443'
+#
+# set nat destination rule 101 description 'HTTP'
+# set nat destination rule 101 destination port '80'
+# set nat destination rule 101 inbound-interface 'eth0'
+# set nat destination rule 101 protocol 'tcp'
+# set nat destination rule 101 translation address "10.10.12.1"
+# set nat destination rule 101 translation port '80'
 
 
 # Force DNS
